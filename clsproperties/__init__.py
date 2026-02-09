6013b4502edb58d273c94acfb0057c5024864a68
Isaac Bell <icb.241208@gmail.com>
2026-02-09 23:18:07 +0000

    Update __init__.py

--- clsproperties/__init__.py
+++ clsproperties/__init__.py
@@ -24,6 +24,94 @@ __all__ = ["classproperty","ClassPropertyMeta"]
 class classproperty:
     """
     Class property object.
+
+    This class defines a `classproperty` descriptor, supporting getter, setter and deleter functions, that aims to emulate the behaviours of Python's built-in `property` class, providing controlled access to class attributes instead of instance attributes. Like `property` objects, `classproperty` objects support use both as a decorator and as a callable.
+
+    ---
+
+    ## Usage / Implementation
+    
+    The `classproperty` class aims to emulate the behaviours of Python's built-in `property` class, providing controlled access to class attributes instead of instance attributes. Like `property` objects, `classproperty` objects support use both as a decorator and as a callable.
+
+    In both cases, access to the `classproperty` attribute, via the class itself or via instances of the class, is mediated by the defined getter, setter and deleter functions.
+
+    Important: For the setter and deleter functions to be triggered when the `classproperty` attribute is accessed via the class itself, the `ClassPropertyMeta` class is required.
+
+    For detailed information on usage and implementation, view the [Documentation](https://github.com/icb08/clsproperties/wiki/Documentation).
+    
+    ### Decorator
+    
+    Like `property` objects, `classproperty` objects can be defined via a decorator defined on the class.
+
+    Example:
+
+    ```
+    class MyClass(metaclass=ClassPropertyMeta):
+        
+        _value = 67
+
+        @classproperty
+        def value(cls):
+            return _value
+        
+        @value.setter
+        def value(cls,newvalue):
+            cls._value = newvalue
+        
+    instance = MyClass()
+
+    >>> MyClass.value
+    ... 67
+    >>> instance.value
+    ... 67
+    ```
+
+    ### Callable
+
+    Like `property` objects, `classproperty` objects can also be instantiated by passing the getter, setter and deleter functions as arguments.
+
+    Example:
+
+    ```
+    class MyClass(metaclass=ClassPropertyMeta):
+        
+        _value = 67
+
+        def get_value(cls):
+            return cls._value
+        
+        def set_value(cls,newvalue):
+            cls._value = newvalue
+
+        value = classproperty(get_value,set_value)
+    
+    instance = MyClass()
+
+    >>> MyClass.value
+    ... 67
+    >>> instance.value
+    ... 67
+    ```
+    
+    ---
+
+    ## Attributes / Properties
+    - **fget** (attribute) : *function*
+    > The getter function of the `classproperty` object.
+    - **fset** (attribute) : *function*
+    > The setter function of the `classproperty` object.
+    - **fdel** (attribute) : *function*
+    > The deleter function of the `classproperty` object.
+
+    ---
+
+    ## Methods / Functions
+    - **getter** (instance method)
+    > Define the getter function of the `classproperty` object.
+    - **setter** (instance method)
+    > Define the setter function of the `classproperty` object.
+    - **deleter** (instance method)
+    > Define the deleter function of the `classproperty` object.
     """
 
     def __init__(self,fget=None,fset=None,fdel=None,doc=None):
