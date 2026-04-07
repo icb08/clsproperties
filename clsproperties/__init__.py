9dafae213f5b89da37fd823b455c4a83f44a8a58
Isaac Bell <icb.241208@gmail.com>
2026-04-08 00:51:11 +0100

    Update __init__.py

--- clsproperties/__init__.py
+++ clsproperties/__init__.py
@@ -153,11 +153,13 @@ class classproperty:
     
 class ClassPropertyMeta(type):
     """
-    Class property metaclass. UNFINISHED DOCSTRING.
+    Class property metaclass.
 
-    This class defines a metaclass, that enables execution of the setter and deleter functionintercepts class attribute assignment and deletion operations and redirects them to the corresponding setter and deleter functions of `classproperty` objects.
-
-    This class should be used as the metaclass for classes which define `classproperty` objects, if the setter and deleter functions of the `classproperty` objects should be executed when accessed via the class itself, instead of overriding the `classproperty` object.
+    This class defines a metaclass, that enables full functionality of `classproperty` objects defined in classes, whose metaclass is `ClassPropertyMeta`, by enabling class-level attribute assignment and deletion operations of `classproperty` objects to execute the corresponding setter and deleter functions of the `classproperty` object, instead of overriding the `classproperty` object. UNFINISHED.
+    
+    Like `property` objects, class-level attribute assignment operations (e.g. `ClassName.class_attribute = x`) would override the `classproperty` object (e.g. if `ClassName.class_attribute` was a `classproperty` object, then `ClassName.class_attribute = x` would replace the `classproperty` object with `x`, instead of executing the setter function of the `classproperty` object). This metaclass intercepts the class attribute assignment operations via the class, and executes the corresponding setter function of the `classproperty` object.
+    
+    Like `property` objects, class-level attribute deletion operations (e.g. `del ClassName.class_attribute`) would delete the `classproperty` object (e.g. if `ClassName.class_attribute` was a `classproperty` object, then `del ClassName.class_attribute` would delete the `classproperty` object with `x`, instead of executing the deleter function of the `classproperty` object). This metaclass intercepts the class attribute deletion operations via the class, and executes the corresponding deleter function of the `classproperty` object.
     
     For detailed information on usage and implementation, view the [Documentation](https://github.com/icb08/clsproperties/wiki/Documentation).
 
