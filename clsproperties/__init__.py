794053cd6c50a993e00e7c973d4564bd7dd39e34
Isaac Bell <icb.241208@gmail.com>
2026-04-08 23:53:59 +0100

    Update __init__.py

--- clsproperties/__init__.py
+++ clsproperties/__init__.py
@@ -1,7 +1,7 @@
 """
 # clsproperties
 - **Author:** [Isaac Bell](https://github.com/icb08)
-- **Version:** [1.0.0](https://github.com/icb08/clsproperties/wiki/Changelog#1-0-0)
+- **Version:** [1.0.1](https://github.com/icb08/clsproperties/wiki/Changelog#1-0-1)
 
 This library provides `classproperty` objects for controlled access to class attributes.
 
@@ -16,7 +16,7 @@ The `classproperty` class aims to emulate the behaviours of Python's built-in `p
 """
 
 __author__ = "Isaac Bell"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = ["classproperty","ClassPropertyMeta"]
 
 class classproperty:
